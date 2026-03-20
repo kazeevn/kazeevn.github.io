3af9e158334acdfa1ca897caf2d6352929152b81
Nikita Kazeev <kazeevn@gmail.com>
2026-03-21 04:12:15 +0800

    Image opt

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const sharedConfig: NextConfig = {
   output: 'export',
   reactCompiler: true,
   images: {
-    unoptimized: true,
+    unoptimized: false,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
